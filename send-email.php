d87f5c4914876430565fcef79a81ca7d12585a40
HappyCat-9487 <lianglu3366@gmail.com>
2023-08-13 17:06:49 +0200

    Update

--- send-email.php
+++ send-email.php
@@ -3,3 +3,23 @@
 $name = $_POST["name"];
 $email = $_POST["email"];
 $message = $_POST["message"];
+
+require "vendor/autoload.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+$mail = new PHPMailer(true);
+
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+
+$mail->Host = "smtp.gmail.com";
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 587;
+
+$mail->Username = "lianglu3585@gmail.com"
+$mail->Password = "HappyCat-9487"
+
+$mail->setForm
+
