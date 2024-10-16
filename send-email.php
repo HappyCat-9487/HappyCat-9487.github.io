1f40da6cba51b790d538f4016ad7e0bcb09e5ece
HappyCat-9487 <lianglu3366@gmail.com>
2024-10-16 15:42:29 +0200

    Update for the email and modify the content

--- send-email.php
+++ send-email.php
@@ -2,34 +2,39 @@
 
 $name = $_POST["name"];
 $email = $_POST["email"];
+$email = $_POST["subject"];
 $message = $_POST["message"];
 
 require "vendor/autoload.php";
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
 $mail = new PHPMailer(true);
 
-$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+try{
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
 
-$mail->isSMTP();
-$mail->SMTPAuth = true;
+    $mail->isSMTP();
+    $mail->Host = "smtp.gmail.com";
+    $mail->SMTPAuth = true;
 
-$mail->Host = "smtp.gmail.com";
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-$mail->Port = 587;
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;
 
-$mail->Username = "ericcool@gmail.com"
-$mail->Password = "tifrnbjthrzhejwt"
+    $mail->Username = "ericcool@gmail.com";
+    $mail->Password = "tifrnbjthrzhejwt";
 
-$mail->setFrom($email, $name);
-$mail->addAddress("lianglu3366@gmail.com", "Mr. Lu")
+    $mail->setFrom($email, $name);
+    $mail->addAddress("lianglu3366@gmail.com", "Mr. Lu");
 
-$mail->Subject = $subject;
-$mail->Body = $message;
+    $mail->Subject = $subject;
+    $mail->Body = $message;
 
-$mail->send();
+    $mail->send();
 
-echo "email sent";
-
+    echo "email sent successfully";
+} catch (Exception $e){
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
