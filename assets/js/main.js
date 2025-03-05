eb228e928d5f240998c38ac7fb2fe78fd373566b
HappyCat-9487 <lianglu3366@gmail.com>
2025-03-05 19:29:44 +0100

    Put the javascript part to the main js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -242,15 +242,16 @@ $(document).ready(function () {
 	});
 });
 
-var submitted = false;
-				
-function showSuccessMessage() {
-	$("#gform").fadeOut(500, function() {
-		$("#success-message").fadeIn(500);
-	});
-}
 
 $(document).ready(function() {
+	var submitted = false;
+					
+	function showSuccessMessage() {
+		$("#gform").fadeOut(500, function() {
+			$("#success-message").fadeIn(500);
+		});
+	}
+	
 	$('#gform').on('submit', function(e) {
 		//Make sure it get sended in a correct way to prevent Javascript block it
 		submitted = true;
